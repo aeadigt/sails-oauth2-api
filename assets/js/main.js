a586a4b6733ea62c07e875c6cce4d48ba0210672
aeadigt <aea@digt.ru>
2016-09-13 17:20:53 +0300

    Добавлено добавление задач

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,140 @@
 
 
         // ********************** Var ********************** //
+        // Приложение
+        window.App = {
+            Models: {},
+            Collections: {},
+            Views: {}
+        };
+
+
+        // ********************** Model ********************** //
+        // Модель задача
+        App.Models.Task = Backbone.Model.extend({
+            validate: function(attrs) {
+                if ( ! $.trim(attrs.title) ) {
+                    return 'Имя задачи должно быть валидным!';
+                }
+            }
+        });
+
+
+        // ********************** Collection ********************** //
+        // Коллекция задач
+        App.Collections.Task = Backbone.Collection.extend({
+            model: App.Models.Task
+        });
+
+
+        // ********************** View ********************** //
+        // Шаблон
+        window.template = function(id) {
+            return _.template( $('#' + id).html() );
+        }
+
+        // Представление одной задачи
+        App.Views.Task = Backbone.View.extend({
+            initialize: function() {
+                this.model.on('change', this.render, this);
+                this.model.on('destroy', this.remove, this);
+            },
+            tagName: 'li',
+            template: template('task_template'),
+            render: function() {
+                var template = this.template( this.model.toJSON() );
+                this.$el.html(template);
+                return this;
+            },
+            events: {
+                'click .edit': 'editTask',
+                'click .delete': 'destroy'
+            },
+            destroy: function() {
+                this.model.destroy();
+            },
+            remove: function() {
+                this.$el.remove();
+            },
+            editTask: function() {
+                var newTaskTitle = prompt( 'Как переименуем задачу?', this.model.get('title') );
+                this.model.set({"title": newTaskTitle}, {validate: true});﻿
+            }
+        });
+
+        // Представление списка задач
+        App.Views.Tasks = Backbone.View.extend({
+            tagName: 'ul',
+            initialize: function() {
+                this.collection.on('add', this.addOne, this);
+            },
+            render: function() {
+                this.collection.each(this.addOne, this);
+                return this;
+            },
+            addOne: function(task) {
+                // Создавать новый дочерний вид
+                var taskView = new App.Views.Task({ model: task });
+
+                // Добавлять его в корневой элемент
+                this.$el.append(taskView.render().el)
+            }
+        });
+
+        // Добавление задач
+        App.Views.AddTask = Backbone.View.extend({
+            el: '#add_task',
+            events: {
+                'submit': 'submit'
+            },
+            initialize: function() {
+            },
+            submit: function(e) {
+                e.preventDefault();
+                var newTaskTitle = $(e.currentTarget).find('input[type=text]').val();
+                var newTask = new App.Models.Task({ title: newTaskTitle });
+                this.collection.add(newTask);
+            }
+        });
+
+
+        // ********************** Create ********************** //
+        // Создание экземпляра коллекции задачи
+        window.tasksCollection = new App.Collections.Task([
+            {
+                title: 'Сходить в магазин',
+                priority: 4
+            },
+            {
+                title: 'Получить почту',
+                priority: 3
+            },
+            {
+                title: 'Сходить на работу',
+                priority: 5
+            }
+        ]);
+
+        // Отрисовка списка задач
+        var tasksView = new App.Views.Tasks({
+            collection: tasksCollection
+        });
+
+        $('.tasks').html(tasksView.render().el);
+
+        var addTaskView = new App.Views.AddTask({ collection: tasksCollection });
+
+
+
+
+
+
+
+
+
+
+        /*
+        // ********************** Var ********************** //
         var app = {
             models: {},
             collections: {}
@@ -83,5 +217,6 @@
         });
 
         getAllModels();
+        */
     });
 })();
